904bb9e7a250cc40d25425f7a5388a57bf5ddf1d
Thomas Lamprecht <t.lamprecht@proxmox.com>
2019-07-24 10:20:20 +0200

    test: create db: drop all active connections before dropping DB
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/tests/create_testdb.pl
+++ src/tests/create_testdb.pl
@@ -18,7 +18,15 @@ my $dbname = "Proxmox_testdb";
 
 if ($list->{$dbname}) {
 
-    print "delete existing test database\n";
+    print "Drop all connections from existing test database '$dbname'\n";
+    my $dbh = PMG::DBTools::open_ruledb($dbname);
+    $dbh->do(
+	"SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity ".
+	"WHERE pg_stat_activity.datname = '$dbname' AND pid <> pg_backend_pid()"
+    );
+    $dbh->disconnect();
+
+    print "delete existing test database '$dbname'\n";
     PMG::DBTools::delete_ruledb($dbname);
 }
 
