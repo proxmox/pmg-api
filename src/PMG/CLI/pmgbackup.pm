dd35bbbd82d7e5b14c0d58a90eaeb9bd02f22656
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-11-18 08:54:21 +0100

    pmgbackup: rework CLI for pbs integration
    Update to new params.
    
    The job is something different than the snapshots itself, so keep
    that split.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/PMG/CLI/pmgbackup.pm
+++ src/PMG/CLI/pmgbackup.pm
@@ -2,15 +2,12 @@ package PMG::CLI::pmgbackup;
 
 use strict;
 use warnings;
-use Data::Dumper;
-use POSIX qw(strftime);
 
 use PVE::Tools;
 use PVE::SafeSyslog;
 use PVE::INotify;
 use PVE::CLIHandler;
 use PVE::CLIFormatter;
-use PVE::JSONSchema qw(get_standard_option);
 use PVE::PTY;
 
 use PMG::RESTEnvironment;
@@ -68,30 +65,32 @@ our $cmddef = {
     backup => [ 'PMG::API2::Backup', 'backup', undef, { node => $nodename } ],
     restore => [ 'PMG::API2::Backup', 'restore', undef, { node => $nodename } ],
     list => [ 'PMG::API2::Backup', 'list', undef, { node => $nodename }, $format_backup_list ],
-    remote => {
-	list => ['PMG::API2::PBS::Remote', 'list', undef, undef,  sub {
+    'proxmox-backup' => {
+	remote => {
+	    list => ['PMG::API2::PBS::Remote', 'list', undef, undef,  sub {
+		my ($data, $schema, $options) = @_;
+		PVE::CLIFormatter::print_api_result($data, $schema, ['remote', 'server', 'datastore', 'username' ], $options);
+	    }, $PVE::RESTHandler::standard_output_options ],
+	    add => ['PMG::API2::PBS::Remote', 'create', ['remote'] ],
+	    remove => ['PMG::API2::PBS::Remote', 'delete', ['remote'] ],
+	    set => ['PMG::API2::PBS::Remote', 'update_config', ['remote'] ],
+	},
+	job => {
+	    create => ['PMG::API2::PBS::Job', 'create_timer', ['remote'], { node => $nodename }],
+	    delete => ['PMG::API2::PBS::Job', 'delete_timer', ['remote'], { node => $nodename }],
+	    show => ['PMG::API2::PBS::Job', 'list_timer', ['remote'], { node => $nodename },  sub {
+		my ($data, $schema, $options) = @_;
+		PVE::CLIFormatter::print_api_result($data, $schema, ['remote', 'schedule', 'delay'], $options);
+	    }, $PVE::RESTHandler::standard_output_options ],
+	},
+	list => ['PMG::API2::PBS::Job', 'get_snapshots', ['remote'] , { node => $nodename },  sub {
 	    my ($data, $schema, $options) = @_;
-	    PVE::CLIFormatter::print_api_result($data, $schema, ['remote', 'server', 'datastore', 'username' ], $options);
+	    PVE::CLIFormatter::print_api_result($data, $schema, ['backup-id', 'backup-time', 'size'], $options);
 	}, $PVE::RESTHandler::standard_output_options ],
-	add => ['PMG::API2::PBS::Remote', 'create', ['remote'] ],
-	remove => ['PMG::API2::PBS::Remote', 'delete', ['remote'] ],
-	set => ['PMG::API2::PBS::Remote', 'update_config', ['remote'] ],
-    },
-    pbsjob => {
-	list_backups => ['PMG::API2::PBS::Job', 'get_snapshots', ['remote'] , { node => $nodename },  sub {
-	    my ($data, $schema, $options) = @_;
-	    PVE::CLIFormatter::print_api_result($data, $schema, ['time', 'size'], $options);
-	}, $PVE::RESTHandler::standard_output_options ],
-	forget => ['PMG::API2::PBS::Job', 'forget_snapshot', ['remote', 'time'], { node => $nodename} ],
-	run => ['PMG::API2::PBS::Job', 'run_backup', ['remote'], { node => $nodename} ],
-	restore => ['PMG::API2::PBS::Job', 'restore', ['remote'], { node => $nodename} ],
-	create => ['PMG::API2::PBS::Job', 'create_timer', ['remote'], { node => $nodename }],
-	delete => ['PMG::API2::PBS::Job', 'delete_timer', ['remote'], { node => $nodename }],
-	schedule => ['PMG::API2::PBS::Job', 'list_timer', ['remote'], { node => $nodename },  sub {
-	    my ($data, $schema, $options) = @_;
-	    PVE::CLIFormatter::print_api_result($data, $schema, ['remote', 'schedule', 'delay'], $options);
-	}, $PVE::RESTHandler::standard_output_options ],
-    },
+	forget => ['PMG::API2::PBS::Job', 'forget_snapshot', ['remote', 'backup-id', 'backup-time'], { node => $nodename} ],
+	backup => ['PMG::API2::PBS::Job', 'run_backup', ['remote'], { node => $nodename} ],
+	restore => ['PMG::API2::PBS::Job', 'restore', ['remote', 'backup-id', 'backup-time'], { node => $nodename} ],
+    }
 };
 
 1;
