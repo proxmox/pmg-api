proxmox/pmg-api

commit 4f06ff8ac21baa5736060221191fb109e20d8e37

utils: do not hardcode postgres version
PMG::Utils::lookup_real_service_name is only called
for translating the service names provided as arguments
to PMG::API2::Nodes::syslog (for fetching the journal
for specific units). Instead of hardcoding the
version getting it with a call to `psql` seems justified.

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/Utils.pm
@@ -710,11 +710,15 @@ sub find_local_network_for_ip {
 
 my $service_aliases = {
     'postfix' =>  'postfix@-',
-    'postgres' => 'postgresql@11-main',
 };
 
 sub lookup_real_service_name {
     my $alias = shift;
+
+    if ($alias eq 'postgres') {
+	my $pg_ver = get_pg_server_version();
+	return "postgresl\@${pg_ver}-main";
+    }
 
     return $service_aliases->{$alias} // $alias;
 }

commit 0a05c9d2de4ecb0094d42e7676fb486c2b1db8e2

utils: fix service_aliases usage
2 errors were introduced in 4f06ff8ac21baa5736060221191fb109e20d8e37:

* a typo in the postgresql service name
* it missed the other uses of the service_name hash, apart from the
  lookup_real_service_name sub.

both fixed here

Reported-by: Martin Maurer <martin@proxmox.com>
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/Utils.pm
@@ -717,7 +717,7 @@ sub lookup_real_service_name {
 
     if ($alias eq 'postgres') {
 	my $pg_ver = get_pg_server_version();
-	return "postgresl\@${pg_ver}-main";
+	return "postgresql\@${pg_ver}-main";
     }
 
     return $service_aliases->{$alias} // $alias;
@@ -735,7 +735,7 @@ sub get_full_service_state {
 	}
     };
 
-    $service = $service_aliases->{$service} // $service;
+    $service = lookup_real_service_name($service);
     PVE::Tools::run_command(['systemctl', 'show', $service], outfunc => $parser);
 
     return $res;
@@ -790,7 +790,7 @@ sub service_cmd {
 	$cmd = 'restart' if $cmd eq 'start';
     }
 
-    $service = $service_aliases->{$service} // $service;
+    $service = lookup_real_service_name($service);
     PVE::Tools::run_command(['systemctl', $cmd, $service]);
 };
 