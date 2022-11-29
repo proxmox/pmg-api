proxmox/pmg-api

commit 6aea70bea4c2a21d763325a112bb9bc015b8dff6

api: tasks: keep early exit if rpcenv is not initialized
while $rpcenv is only used in the end to set_result_attrib, the get()
call can cause an early exit if the RESTEnvironment is not
initialized. keep it that way (as with most/all API calls)

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/API2/Tasks.pm
@@ -275,6 +275,8 @@ __PACKAGE__->register_method({
 	my ($task, $filename) = PVE::Tools::upid_decode($param->{upid}, 1);
 	raise_param_exc({ upid => "unable to parse worker upid" }) if !$task;
 
+	my $restenv = PMG::RESTEnvironment->get();
+
 	if ($param->{download}) {
 	    die "Parameter 'download' can't be used with other parameters\n"
 		if (defined($param->{start}) || defined($param->{limit}));
@@ -305,7 +307,7 @@ __PACKAGE__->register_method({
 
 	    my ($count, $lines) = PVE::Tools::dump_logfile($filename, $start, $limit);
 
-	    PMG::RESTEnvironment->get()->set_result_attrib('total', $count);
+	    $rpcenv->set_result_attrib('total', $count);
 
 	    return $lines;
 	}

commit 9bbdd5d5231634f3b222aeb9bb18ec5fc115e88d

api: tasks fix misnamed variable
issue introduced in previous commit:
6aea70bea4c2a21d763325a112bb9bc015b8dff6

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/API2/Tasks.pm
@@ -307,7 +307,7 @@ __PACKAGE__->register_method({
 
 	    my ($count, $lines) = PVE::Tools::dump_logfile($filename, $start, $limit);
 
-	    $rpcenv->set_result_attrib('total', $count);
+	    $restenv->set_result_attrib('total', $count);
 
 	    return $lines;
 	}