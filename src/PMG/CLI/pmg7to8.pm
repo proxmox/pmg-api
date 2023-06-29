d82cdcf02c23cfbf6e03afa91501b312dab10350
Fiona Ebner <f.ebner@proxmox.com>
2023-06-29 10:11:06 +0200

    pmg7to8: have repository check handle -debug suites gracefully
    instead of marking them as unusual suites.
    
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

--- src/PMG/CLI/pmg7to8.pm
+++ src/PMG/CLI/pmg7to8.pm
@@ -385,7 +385,7 @@ sub check_apt_repos {
 	    }
 	    my $where = "in ${file}:${number}";
 
-	    $suite =~ s/-(?:(?:proposed-)?updates|backports|security)$//;
+	    $suite =~ s/-(?:(?:proposed-)?updates|backports|security|debug)(?:-debug)?$//;
 	    if ($suite ne $old_suite && $suite ne $new_suite) {
 		log_notice(
 		    "found unusual suite '$suite', neither old '$old_suite' nor new '$new_suite'.."
