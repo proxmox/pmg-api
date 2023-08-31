16d2ff9f8e90db64114a66d78672f5a03f5ee990
Maximiliano Sandoval <m.sandoval@proxmox.com>
2023-08-31 16:41:13 +0200

    utils: check if file changed before reusing its hash
    We cache the hash of this file, it makes sense to first check if the
    file changed via `stat` and recompute the hash if needed.
    
    Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>

--- src/PMG/Utils.pm
+++ src/PMG/Utils.pm
@@ -1353,14 +1353,22 @@ sub scan_journal_for_rbl_rejects {
 }
 
 my $hwaddress;
+my $hwaddress_st = {};
 
 sub get_hwaddress {
-
-    return $hwaddress if defined ($hwaddress);
-
     my $fn = '/etc/ssh/ssh_host_rsa_key.pub';
+    my $st = stat($fn);
+
+    if (defined($hwaddress)
+	&& $hwaddress_st->{mtime} == $st->mtime
+	&& $hwaddress_st->{ino} == $st->ino
+	&& $hwaddress_st->{dev} == $st->dev) {
+	return $hwaddress;
+    }
+
     my $sshkey = PVE::Tools::file_get_contents($fn);
     $hwaddress = uc(Digest::MD5::md5_hex($sshkey));
+    $hwaddress_st->@{'mtime', 'ino', 'dev'} = ($st->mtime, $st->ino, $st->dev);
 
     return $hwaddress;
 }
