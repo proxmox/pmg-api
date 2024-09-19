06d798a311b856b2944d5eb62d461d43f746856e
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2024-09-19 15:19:38 +0200

    api: journal/task: annotate as download endpoint
    to satisfy the stricter checks in pve-http-server.
    
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
    Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
    Tested-by:  Dominik Csapak <d.csapak@proxmox.com>

--- src/PMG/API2/Nodes.pm
+++ src/PMG/API2/Nodes.pm
@@ -292,6 +292,7 @@ __PACKAGE__->register_method({
     proxyto => 'node',
     permissions => { check => [ 'admin', 'audit' ] },
     protected => 1,
+    download => 1,
     parameters => {
 	additionalProperties => 0,
 	properties => {
