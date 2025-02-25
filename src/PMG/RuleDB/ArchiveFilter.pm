proxmox/pmg-api

commit c320d56091a8b5aa536c4d15e82d063b66ea9da9

utils: content-type: don't fallback to header information for magic
file-type detection based on content/magic is the single piece of
information not determined by the headers of the e-mail, and thus not
directly controlled by the sender.

this patch removes the fallback to the content-type header mime-type
in case magic_mime_type_for_file does not detect the type.

one exception to this is trying to eagerly gain information from
archives - where we want to try to unpack an archive if the header
says it is an archive but the content is not detected as such.

Reported-by: Friedrich Weber <f.weber@proxmox.com>
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/RuleDB/ArchiveFilter.pm
@@ -59,7 +59,7 @@ sub parse_entity {
     if (my $id = $entity->head->mime_attr ('x-proxmox-tmp-aid')) {
 	chomp $id;
 
-	my $header_ct = $entity->head->mime_attr ('content-type');
+	my $header_ct = $entity->{PMX_header_ct};
 
 	my $magic_ct = $entity->{PMX_magic_ct};
 

src/PMG/RuleDB/ContentTypeFilter.pm
@@ -72,7 +72,7 @@ sub parse_entity {
     if (my $id = $entity->head->mime_attr ('x-proxmox-tmp-aid')) {
 	chomp $id;
 
-	my $header_ct = $entity->head->mime_attr ('content-type');
+	my $header_ct = $entity->{PMX_header_ct};
 
 	my $magic_ct = $entity->{PMX_magic_ct};
 

src/PMG/Utils.pm
@@ -598,7 +598,7 @@ sub magic_mime_type_for_file {
     my $bufsize = Xdgmime::xdg_mime_get_max_buffer_extents();
     die "got strange value for max_buffer_extents" if $bufsize > 4096*10;
 
-    my $ct = "application/octet-stream";
+    my $ct;
 
     my $fh = IO::File->new("<$filename") ||
 	die "unable to open file '$filename' - $!";
@@ -611,6 +611,7 @@ sub magic_mime_type_for_file {
 
     die "unable to read file '$filename' - $!" if ($len < 0);
 
+    $ct ||= "application/octet-stream";
     return $ct;
 }
 
@@ -619,14 +620,9 @@ sub add_ct_marks {
 
     if (my $path = $entity->{PMX_decoded_path}) {
 
-	# set a reasonable default if magic does not give a result
-	$entity->{PMX_magic_ct} = $entity->head->mime_attr('content-type');
+	$entity->{PMX_header_ct} = $entity->head->mime_attr('content-type');
 
-	if (my $ct = magic_mime_type_for_file($path)) {
-	    if ($ct ne 'application/octet-stream' || !$entity->{PMX_magic_ct}) {
-		$entity->{PMX_magic_ct} = $ct;
-	    }
-	}
+	$entity->{PMX_magic_ct} = magic_mime_type_for_file($path);
 
 	my $filename = $entity->head->recommended_filename;
 	$filename = basename($path) if !defined($filename) || $filename eq '';

src/bin/pmg-smtp-filter
@@ -561,9 +561,15 @@ sub run_dequeue {
 sub unpack_entity {
     my ($self, $unpack, $entity, $msginfo, $queue) = @_;
 
-    my ($magic, $path) = $entity->@{'PMX_magic_ct', 'PMX_decoded_path'};
+    my ($magic, $headerct, $path) = $entity->@{'PMX_magic_ct', 'PMX_header_ct', 'PMX_decoded_path'};
 
     if ($magic && $path) {
+	# in order to not miss information from a misdetected archive use information provided in the
+	# header here as well
+	if ($headerct && ($magic && $magic eq 'application/octet-stream')) {
+	    $magic = $headerct;
+	}
+
 	my $filename = basename ($path);
 
 	if (PMG::Unpack::is_archive ($magic)) {

commit 178143df664c9b1f32564eab62108f914085416d

ruledb: content-type: do not rely on PMX_header_ct
As noted in the cover-letter for v3 of the series introducing this [0]
the PMX_magic_ct, PMX_glob_ct and PMX_header_ct are only set if there
is a bodyhandle for a mime-part, which only exists if the part has no
sub-parts.

[0]: https://lore.proxmox.com/pmg-devel/74a089b6-d858-4525-9822-da58b9c8f4d7@proxmox.com/T/#t

However we want to match on content-types even for mail-parts which
have no bodyhandle:
* our internal regression-test suite has a mail with `message/partial`
  set
* there are probably sensible uses for matching `multipart/related`
  parts (e.g. when trying to get a negated match.

Noticed during a check of our regression-tests.

Fixes: c320d56 ("utils: content-type: don't fallback to header information for magic")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Dominik Csapak <d.csapak@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PMG/RuleDB/ArchiveFilter.pm
@@ -59,7 +59,7 @@ sub parse_entity {
     if (my $id = $entity->head->mime_attr ('x-proxmox-tmp-aid')) {
 	chomp $id;
 
-	my $header_ct = $entity->{PMX_header_ct};
+	my $header_ct = $entity->head->mime_attr ('content-type');
 
 	my $magic_ct = $entity->{PMX_magic_ct};
 

src/PMG/RuleDB/ContentTypeFilter.pm
@@ -114,7 +114,7 @@ sub parse_entity {
     if (my $id = $entity->head->mime_attr ('x-proxmox-tmp-aid')) {
 	chomp $id;
 
-	my $header_ct = $entity->{PMX_header_ct};
+	my $header_ct = $entity->head->mime_attr ('content-type');
 
 	my $magic_ct = $entity->{PMX_magic_ct};
 