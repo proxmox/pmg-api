proxmox/pmg-api

commit 9665bbc605e30fd4a1ace9f00d551425deb20abc

utils: user schema: explicitly forbid @ in user-names
PMGs terms are:
* 'userid' consists of 'username'@'realm'

without this patch it was possible to create a user through the api,
with @ in the username ('foo@bar@pmg'), and it got written to the
user-conf.
Reading that entry was not possible, as the verification on read was
stricter.

This patch forbids '@' in usernames, and additionally drops the
maxLength of 64, as 60 are already enforced by the regex pattern match
(leaving 4 as minimal length for '@pmg'/'@pam').

Potential for regression should be minimal (the users could not be
read-back from the config).

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/Utils.pm
@@ -49,7 +49,7 @@ postgres_admin_cmd
 try_decode_utf8
 );
 
-my $user_regex = qr![^\s:/]+!;
+my $user_regex = qr![^\s:@/]+!;
 
 PVE::JSONSchema::register_standard_option('pmg-starttime', {
     description => "Only consider entries newer than 'starttime' (unix epoch). Default is 'now - 1day'.",
@@ -103,7 +103,6 @@ PVE::JSONSchema::register_standard_option('username', {
     description => "Username (without realm)",
     type => 'string',
     pattern => '[^\s:\/\@]{1,60}',
-    maxLength => 64,
 });
 
 PVE::JSONSchema::register_standard_option('pmg-email-address', {

commit 6b8c7457d69e8016325b9fcda79b54d9800c349c

utils: verify_username: allow quarantine logins again
verify_username is used in many places to split into realms (the part
after the last '@') and usernames (everthing before).

The commit disallowing '@' in usernames broke quarantine login
(users login with `localpart@domainname.com@quarantine`)

Fixes: 9665bbc ("utils: user schema: explicitly forbid @ in user-names")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/API2/Users.pm
@@ -126,6 +126,8 @@ __PACKAGE__->register_method ({
 	    my ($userid, $username, $realm) = PMG::Utils::verify_username($entry->{userid});
 	    die "invalid realm '$realm' in userid\n" if !PMG::Auth::Plugin::is_valid_realm($realm);
 
+	    die "'@' forbidden in username\n" if $username =~/@/;
+
 	    if ($entry->{realm}) {
 		die "realm parameter does not fit userid ('$entry->{realm}' != '$realm')\n"
 		    if $entry->{realm} ne $realm;

src/PMG/Utils.pm
@@ -49,7 +49,7 @@ postgres_admin_cmd
 try_decode_utf8
 );
 
-my $user_regex = qr![^\s:@/]+!;
+my $user_regex = qr![^\s:/]+!;
 
 PVE::JSONSchema::register_standard_option('pmg-starttime', {
     description => "Only consider entries newer than 'starttime' (unix epoch). Default is 'now - 1day'.",