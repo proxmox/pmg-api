proxmox/pmg-api

commit 51f03aee7b0aee88aabfb53e71202d2529248c13

ruledb: updatedb: automatically adapt to new .exe mime-type
by updating the row to application/x-msdownload and inserting a new
row for 'application/vnd.microsoft.portable-executable'

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PMG/DBTools.pm
@@ -574,6 +574,37 @@ sub upgradedb {
                 . " AND value = 'content-type:application/x-java-vm';");
     };
 
+    eval {
+        my $insert_sth =
+            $dbh->prepare(
+                "INSERT INTO Object (objecttype, objectgroup_id, value) VALUES (?, ?, ?)");
+        my $select_sth =
+            $dbh->prepare(
+                "SELECT objecttype, objectgroup_id FROM object WHERE objecttype IN (3003, 3005)"
+                . " AND value = 'content-type:application/x-ms-dos-executable'");
+
+        $dbh->begin_work();
+        $select_sth->execute();
+        while (my $ref = $select_sth->fetchrow_hashref()) {
+            $insert_sth->execute(
+                $ref->{objecttype},
+                $ref->{objectgroup_id},
+                "application/vnd\.microsoft\.portable-executable",
+            );
+        }
+
+        $dbh->do(
+            "UPDATE Object SET value = 'content-type:application/x-msdownload' WHERE objecttype in (3003, 3005)"
+                . " AND value = 'content-type:application/x-ms-dos-executable';");
+
+        $dbh->commit();
+    };
+
+    if (my $err = $@) {
+        $dbh->rollback;
+        die $err;
+    }
+
     # increase column size of cgreylist.ipnet for ipv6 support and transfer data
     eval {
         my $sth =

commit e1b1e3fbf24980bf00d878cf01303ca993668d83

ruledb: updatedb: fix missing 'content-type:' prefix on update
The recent change to unconditionally add
"application/vnd.microsoft.portable-executable" and replace
"application/x-ms-dos-executable" with "application/x-msdownload"

missed adding the 'content-type:' prefix to the former.

This resulted in errors in the journal after upgrading:
```
unable to load rulecache : undefined object attribute: ERROR at ...
```

While testing I verified that resetting the RuleDB to defaults (`pmgdb
init` or via GUI) also fixes the issue (but this is only viable for
non-modified rulesets).

Fixes:  51f03aee ("ruledb: updatedb: automatically adapt to new .exe mime-type")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/DBTools.pm
@@ -589,7 +589,7 @@ sub upgradedb {
             $insert_sth->execute(
                 $ref->{objecttype},
                 $ref->{objectgroup_id},
-                "application/vnd\.microsoft\.portable-executable",
+                "content-type:application/vnd\.microsoft\.portable-executable",
             );
         }
 