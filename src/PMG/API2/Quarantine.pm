proxmox/pmg-api

commit ba7a7855a512afab591f51ba7040b97ac35293e8

api: quarantine: re-add black- whitelist api methods for compatibilty
Modifications to the spam-report templates might not get updated to the
new pharsing, and would break the functionality of the user
welcome-/blacklist links in those reports.
copy the methods with the previous names and keep them for one major
release cycle.

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PMG/API2/Quarantine.pm
@@ -420,6 +420,199 @@ __PACKAGE__->register_method({
     },
 });
 
+# FIXME: drop white-/blacklist compatibility calls with PMG 10
+
+__PACKAGE__->register_method({
+    name => 'whitelist',
+    path => 'whitelist',
+    method => 'GET',
+    proxyto => 'master',
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    description => "Show user welcomelist.",
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+        },
+    },
+    returns => {
+        type => 'array',
+        items => {
+            type => "object",
+            properties => {
+                address => {
+                    type => "string",
+                },
+            },
+        },
+    },
+    code => sub {
+        my ($param) = @_;
+
+        return $read_or_modify_user_bw_list->('WL', $param);
+    },
+});
+
+__PACKAGE__->register_method({
+    name => 'whitelist_add',
+    path => 'whitelist',
+    method => 'POST',
+    proxyto => 'master',
+    description => "Add user welcomelist entries.",
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    protected => 1,
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+            address => get_standard_option(
+                'pmg-welcomeblocklist-entry-list',
+                {
+                    description => "The address you want to add.",
+                },
+            ),
+        },
+    },
+    returns => { type => 'null' },
+    code => sub {
+        my ($param) = @_;
+
+        my $addresses = [split(',', $param->{address})];
+        $read_or_modify_user_bw_list->('WL', $param, $addresses);
+
+        return undef;
+    },
+});
+
+__PACKAGE__->register_method({
+    name => 'whitelist_delete_base',
+    path => 'whitelist',
+    method => 'DELETE',
+    proxyto => 'master',
+    description => "Delete user welcomelist entries.",
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    protected => 1,
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+            address => get_standard_option(
+                'pmg-welcomeblocklist-entry-list',
+                {
+                    pattern => '',
+                    description =>
+                        "The address, or comma-separated list of addresses, you want to remove.",
+                },
+            ),
+        },
+    },
+    returns => { type => 'null' },
+    code => sub {
+        my ($param) = @_;
+
+        my $addresses = [split(',', $param->{address})];
+        $read_or_modify_user_bw_list->('WL', $param, $addresses, 1);
+
+        return undef;
+    },
+});
+
+__PACKAGE__->register_method({
+    name => 'blacklist',
+    path => 'blacklist',
+    method => 'GET',
+    proxyto => 'master',
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    description => "Show user blocklist.",
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+        },
+    },
+    returns => {
+        type => 'array',
+        items => {
+            type => "object",
+            properties => {
+                address => {
+                    type => "string",
+                },
+            },
+        },
+    },
+    code => sub {
+        my ($param) = @_;
+
+        return $read_or_modify_user_bw_list->('BL', $param);
+    },
+});
+
+__PACKAGE__->register_method({
+    name => 'blacklist_add',
+    path => 'blacklist',
+    method => 'POST',
+    proxyto => 'master',
+    description => "Add user blocklist entries.",
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    protected => 1,
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+            address => get_standard_option(
+                'pmg-welcomeblocklist-entry-list',
+                {
+                    description => "The address you want to add.",
+                },
+            ),
+        },
+    },
+    returns => { type => 'null' },
+    code => sub {
+        my ($param) = @_;
+
+        my $addresses = [split(',', $param->{address})];
+        $read_or_modify_user_bw_list->('BL', $param, $addresses);
+
+        return undef;
+    },
+});
+
+__PACKAGE__->register_method({
+    name => 'blacklist_delete_base',
+    path => 'blacklist',
+    method => 'DELETE',
+    proxyto => 'master',
+    description => "Delete user blocklist entries.",
+    permissions => { check => ['admin', 'qmanager', 'audit', 'quser'] },
+    protected => 1,
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            pmail => $pmail_param_type,
+            address => get_standard_option(
+                'pmg-welcomeblocklist-entry-list',
+                {
+                    pattern => '',
+                    description =>
+                        "The address, or comma-separated list of addresses, you want to remove.",
+                },
+            ),
+        },
+    },
+    returns => { type => 'null' },
+    code => sub {
+        my ($param) = @_;
+
+        my $addresses = [split(',', $param->{address})];
+        $read_or_modify_user_bw_list->('BL', $param, $addresses, 1);
+
+        return undef;
+    },
+});
+# FIXME: END: drop white-/blacklist compatibility calls with PMG 10
+
 my $quar_type_map = {
     spam => 'S',
     attachment => 'A',

commit aa12458a9cfc6c84b661ae25562d38f8c8e9b6ba

api: quarantine: fix wrong backward-compatibility action
While renaming to blocklist/welcomelist we kept the old
methods in the API for backward compatibilty, and added
both options as equivalent actions.
However it seems I substituted the backward compatibility name, as
well at some point and did not notice it.

Additionally the commits of renaming and re-adding the old names for
backward compatibility were not separated cleanly (thus this
patch fixes 2 commits).

Reported in our German community forum:
https://forum.proxmox.com/threads/.173910/

Tested by reproducing with a spamreport template with the old actions,
and verified that the welcomelist action works with this patch.

Fixes: ba7a7855 ("api: quarantine: re-add black- whitelist api methods for compatibilty")
Fixes: 85981ebd ("api: quarantine: rename to block/welcomelist")
Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Dominik Csapak <d.csapak@proxmox.com>
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/API2/Quarantine.pm
@@ -1433,7 +1433,7 @@ __PACKAGE__->register_method({
             my $pmail = try_decode_utf8($ref->{pmail});
             my $receiver = try_decode_utf8($ref->{receiver} // $ref->{pmail});
 
-            if ($action eq 'welcomelist' || $action eq 'welcomelist') {
+            if ($action eq 'welcomelist' || $action eq 'whitelist') {
                 PMG::Quarantine::add_to_blockwelcome($dbh, $pmail, 'WL', [$sender]);
                 PMG::Quarantine::deliver_quarantined_mail($dbh, $ref, $receiver);
             } elsif ($action eq 'blocklist' || $action eq 'blacklist') {