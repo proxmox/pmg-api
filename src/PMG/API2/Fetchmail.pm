b86581fbebd7efb5918643c2b92c58ff9108d018
Thomas Lamprecht <t.lamprecht@proxmox.com>
2026-02-09 10:04:20 +0100

    re-tidy source code
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/PMG/API2/Fetchmail.pm
+++ src/PMG/API2/Fetchmail.pm
@@ -203,7 +203,7 @@ __PACKAGE__->register_method({
             $fmcfg->{$id} = $entry;
 
             PVE::INotify::write_file('fetchmailrc', $fmcfg);
-            PMG::Utils::service_cmd('fetchmail', 'restart')
+            PMG::Utils::service_cmd('fetchmail', 'restart');
         };
 
         PMG::Config::lock_config($code, "update fechtmail configuration failed");
@@ -243,7 +243,7 @@ __PACKAGE__->register_method({
             }
 
             PVE::INotify::write_file('fetchmailrc', $fmcfg);
-            PMG::Utils::service_cmd('fetchmail', 'restart')
+            PMG::Utils::service_cmd('fetchmail', 'restart');
         };
 
         PMG::Config::lock_config($code, "update fechtmail configuration failed");
@@ -282,7 +282,7 @@ __PACKAGE__->register_method({
             delete $fmcfg->{$id};
 
             PVE::INotify::write_file('fetchmailrc', $fmcfg);
-            PMG::Utils::service_cmd('fetchmail', 'restart')
+            PMG::Utils::service_cmd('fetchmail', 'restart');
         };
 
         PMG::Config::lock_config($code, "delete fechtmail configuration failed");
