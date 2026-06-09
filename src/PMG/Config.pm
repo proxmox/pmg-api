proxmox/pmg-api

commit de0ba6851b2dfc14ae89d124f0bb07aef6f38d18

fix #3991: record changes to the smtp-welcomelist and reload postfix
adding an entry to the postfix welcomelist needs a reload of postfix,
in order to become effective immediately.

the writer sub already reflects if a change happened in its return
value, we simply did not track that when deciding if postfix needs
reloading.

PMG::RuleDB::reload_ruledb is called with an argument only for the
smtp-welcomelist (which is a special case of ruledb object), for
regular objects it's called without (the API calls are generated in
all cases via PMG::RuleDB::Object::register_api).

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260313201338.18613-1-s.ivanov@proxmox.com

src/PMG/Config.pm
@@ -1842,10 +1842,13 @@ sub rewrite_postfix_welcomelist {
         }
     }
 
-    $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
-    $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
-    $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
-    $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+    my $changes = 0;
+    $changes ||= $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
+    $changes ||= $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
+    $changes ||= $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
+    $changes ||= $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+
+    return $changes;
 }
 
 # rewrite /etc/postfix/*
@@ -1873,7 +1876,7 @@ sub rewrite_config_postfix {
     postmap_tls_policy();
     postmap_tls_inbound_domains();
 
-    rewrite_postfix_welcomelist($rulecache) if $rulecache;
+    $changes ||= rewrite_postfix_welcomelist($rulecache) if $rulecache;
 
     # make sure aliases.db is up to date
     system('/usr/bin/newaliases');

src/PMG/DBTools.pm
@@ -1437,7 +1437,8 @@ sub reload_ruledb {
     if (defined($ruledb)) {
         eval {
             my $rulecache = PMG::RuleCache->new($ruledb);
-            PMG::Config::rewrite_postfix_welcomelist($rulecache);
+            my $changed = PMG::Config::rewrite_postfix_welcomelist($rulecache);
+            PMG::Utils::service_cmd('postfix', 'reload') if $changed;
         };
         warn "problems updating SMTP welcomelist - $@" if $@;
     }

commit 17f17bced10341069add8a53a9466780b0129a38

config: do not shortcircuit smtp-welcomelist generation
combining the results with ||= causes the write_smtp_welcomelist calls
not to be run, after the first one signals a change.
gather the combined changes in a different variable, to ensure each
config-file is actually created/written, as postfix refuses to run if
its config files are missing.

Fixes: de0ba685 ("fix #3991: record changes to the smtp-welcomelist and reload postfix")
Reported-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Dominik Csapak <d.csapak@proxmox.com>
Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260609090517.828636-1-s.ivanov@proxmox.com
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/Config.pm
@@ -1878,10 +1878,15 @@ sub rewrite_postfix_welcomelist {
     }
 
     my $changes = 0;
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+    my $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+    $changes ||= $ret;
 
     return $changes;
 }