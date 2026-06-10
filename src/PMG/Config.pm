proxmox/pmg-api

commit 17f17bced10341069add8a53a9466780b0129a38

config: do not shortcircuit smtp-welcomelist generation
combining the results with ||= causes the write_smtp_welcomelist calls
not to be run, after the first one signals a change.
gather the combined changes in a different variable, to ensure each
config-file is actually created/written, as postfix refuses to run if
its config files are missing.

Fixes: de0ba685 ("fix #3991: record changes to the smtp-welcomelist and reload postfix")
Reported-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Dominik Csapak <d.csapak@proxmox.com>
Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260609090517.828636-1-s.ivanov@proxmox.com
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

src/PMG/Config.pm
@@ -1878,10 +1878,15 @@ sub rewrite_postfix_welcomelist {
     }
 
     my $changes = 0;
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
-    $changes ||= $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+    my $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
+    $changes ||= $ret;
+    $ret = $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
+    $changes ||= $ret;
 
     return $changes;
 }

commit 02f0186c63e984e2b423f21fec3419eba35efebd

config: smtp-welcomelist: avoid short-circuiting due to side-effects
the version with the post-if also reads a bit nicer to me.

a similar issue was fixed in 2022:
991d8f1c ("pmg-daily: avoid short-circuting update of local channels")

minimally tested by:
* removing all smtp-welcomelist files in /etc/postfix and their
  postmap .db result (senderaccess rcptaccess clientaccess
  postscreen_access)
* manually modifying /etc/postfix/master.cf (adding a -v to smtpd)
* adding an entry to the Mail Proxy Welcomelist in the GUI
* checking that all files were created and that postfix was reloaded

Fixes: 17f17bce ("config: do not shortcircuit smtp-welcomelist generation")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260610091356.958395-1-s.ivanov@proxmox.com

src/PMG/Config.pm
@@ -1878,15 +1878,10 @@ sub rewrite_postfix_welcomelist {
     }
 
     my $changes = 0;
-    my $ret;
-    $ret = $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
-    $changes ||= $ret;
-    $ret = $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
-    $changes ||= $ret;
-    $ret = $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
-    $changes ||= $ret;
-    $ret = $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
-    $changes ||= $ret;
+    $changes = 1 if $write_smtp_welcomelist->("/etc/postfix/senderaccess", $fromlist);
+    $changes = 1 if $write_smtp_welcomelist->("/etc/postfix/rcptaccess", $tolist);
+    $changes = 1 if $write_smtp_welcomelist->("/etc/postfix/clientaccess", $clientlist);
+    $changes = 1 if $write_smtp_welcomelist->("/etc/postfix/postscreen_access", $clientlist, 'permit');
 
     return $changes;
 }